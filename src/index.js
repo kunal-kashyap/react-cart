cdeb4372bfc9443fa2e0b10855e0b442e13ad57b
Kunal Kashyap <kunal.kashyap@tothenew.com>
2017-10-15 21:24:50 +0530

    initital commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Cart from './Cart';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<Cart />, document.getElementById('root'));
+registerServiceWorker();
